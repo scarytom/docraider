cdce30f05dc62dfeb24a3ab83d11020e033d6161
scarytom <t.denley@cantab.net>
2013-03-04 23:18:43 +0000

    some static resources

--- src/docraider/handler.clj
+++ src/docraider/handler.clj
@@ -1,10 +1,12 @@
 (ns docraider.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :as resp]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resp/redirect "/index.html"))
+  (fn [req] (if-let [resp ((route/resources "/") req)] (resp/charset resp "UTF-8")))
   (route/not-found "Not Found"))
 
 (def app
