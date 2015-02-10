39bda8005bb292754d257175f1bc02abade6e644
Tom Denley <t.denley@cantab.net>
2015-02-10 19:30:26 +0000

    bring libraries up-to-date

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "A catalogue for your documents"
   :url "http://github.com/netmelody/docraider"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.0"]
-                 [ring/ring-jetty-adapter "1.3.0"]
-                 [compojure "1.1.8"]
-                 [cheshire "5.3.1"]
-                 [org.apache.lucene/lucene-core "4.9.0"]
-                 [org.apache.lucene/lucene-queryparser "4.9.0"]
-                 [org.apache.lucene/lucene-analyzers-common "4.9.0"]]
-  :plugins [[lein-ring "0.8.11"]]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [compojure "1.3.1"]
+                 [cheshire "5.4.0"]
+                 [org.apache.lucene/lucene-core "4.10.3"]
+                 [org.apache.lucene/lucene-queryparser "4.10.3"]
+                 [org.apache.lucene/lucene-analyzers-common "4.10.3"]]
+  :plugins [[lein-ring "0.9.1"]]
   :ring {:handler docraider.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
